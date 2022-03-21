0c6ebe8e73a1f1ba89cbff7f57815356dfde3d27
50no <463823721@qq.com>
2022-03-21 23:20:30 +0800

    cart空架子

---
+++ VueProjects/code-cart/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <EsHeader></EsHeader>
+      <h1>this is app.vue</h1>
+  </div>
+</template>
+
+<script>
+import EsHeader from './components/es-header/EsHeader.vue'
+
+export default {
+  name:'MyApp',
+  components: {
+    EsHeader
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
